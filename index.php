cb5d591273ab032bad7020b4255aab01487cbb67
marco987 <pacemarco87@gmail.com>
2019-09-20 15:34:05 +0200

    Definizione class Parallelepipedo

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
+  class Parallelepipedo {
 
+    public $x;
+    public $y;
+    public $z;
+
+    public function __construct($x, $y, $z) {
+
+      $this -> x = $x;
+      $this -> y = $y;
+      $this -> z = $z;
+    }
+
+    public function getArea() {
+
+      $area = 2 * ($this -> x * $this -> y + $this -> x * $this -> z + $this -> y * $this -> z);
+    }
+
+    public function getVolume() {
+
+      $volume = $this -> x * $this -> y * $this -> z;
+    }
+  };
+
+  // $numero = rand(0,100);
 
 ?>
